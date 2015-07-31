e39af88b4d01fcc88bab765e55df23d2e0c5cbaa
TrustHage <mick.c@live.nl>
2015-07-31 11:53:58 +0200

    ik heb wat veranderd

--- McRaidCraftTNT/src/me/trusthage/tnt/ConfigReload.java
+++ McRaidCraftTNT/src/me/trusthage/tnt/ConfigReload.java
@@ -1,35 +1,29 @@
-/*    */ package me.trusthage.tnt;
-/*    */ 
-/*    */ import org.bukkit.ChatColor;
-/*    */ import org.bukkit.command.Command;
-/*    */ import org.bukkit.command.CommandExecutor;
-/*    */ import org.bukkit.command.CommandSender;
-/*    */ import org.bukkit.entity.Player;
-/*    */ 
-/*    */ 
-/*    */ public class ConfigReload
-/*    */   implements CommandExecutor
-/*    */ {
-/*    */   public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3)
-/*    */   {
-/* 15 */     if (!(arg0 instanceof Player))
-/*    */     {
-/* 17 */       return false;
-/*    */     }
-/*    */     
-/* 20 */     Player player = (Player)arg0;
-/* 21 */     if (player.hasPermission("mcraidcraft.reload"))
-/*    */     {
-/*    */ 
-/* 24 */       Main.plugin.reloadConfig();
-/* 25 */       player.sendMessage(ChatColor.GREEN + "The config file has been reloaded!");
-/*    */     }
-/* 27 */     return false;
-/*    */   }
-/*    */ }
-
-
-/* Location:              C:\Users\GamePC Mick\Desktop\McRaidCraft Spigot 2\plugins\McRaidCraftTNT.jar!\me\trusthage\tnt\ConfigReload.class
- * Java compiler version: 7 (51.0)
- * JD-Core Version:       0.7.1
- */
+     package me.trusthage.tnt;
+     
+     import org.bukkit.ChatColor;
+     import org.bukkit.command.Command;
+     import org.bukkit.command.CommandExecutor;
+     import org.bukkit.command.CommandSender;
+     import org.bukkit.entity.Player;
+     
+     
+     public class ConfigReload
+       implements CommandExecutor
+     {
+       public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3)
+       {
+       if (!(arg0 instanceof Player))
+         {
+         return false;
+         }
+         
+       Player player = (Player)arg0;
+       if (player.hasPermission("mcraidcraft.reload"))
+         {
+     
+         Main.plugin.reloadConfig();
+         player.sendMessage(ChatColor.GREEN + "The config file has been reloaded!");
+         }
+       return false;
+       }
+     }
